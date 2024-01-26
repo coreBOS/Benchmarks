2c083314fd609d580d91f86d2480343a36c29a03
Joe Bordes <joe@tsolucio.com>
2024-01-26 19:35:17 +0100

    (Events) cbEvenHandler action

---
+++ include/events/cbEventHandler.php
@@ -0,0 +1,39 @@
+<?php
+include_once 'build/evBench/loadcorebos.php';
+
+class cbEventHandlerBench {
+
+	private $usrdota0x = 5; // testdmy 2 decimal places > non admin
+
+	/**
+	* @Revs(1000)
+	* @Iterations(5)
+	* @Assert("mode(variant.time.avg) < 3 ms")
+	*/
+	public function benchdoActionAdmin() {
+		global $current_user, $currentModule;
+		$currentModule = 'Accounts';
+		$current_user = Users::getActiveAdminUser();
+		ob_start();
+		cbEventHandler::do_action('corebos.header');
+		cbEventHandler::do_action('corebos.footer');
+		ob_end_clean();
+	}
+
+	/**
+	* @Revs(1000)
+	* @Iterations(5)
+	* @Assert("mode(variant.time.avg) < 4 ms")
+	*/
+	public function benchdoActionNonAdmin() {
+		global $current_user, $currentModule;
+		$currentModule = 'Accounts';
+		$user = new Users();
+		$user->retrieveCurrentUserInfoFromFile($this->usrdota0x);
+		ob_start();
+		cbEventHandler::do_action('corebos.header');
+		cbEventHandler::do_action('corebos.footer');
+		ob_end_clean();
+		$current_user = Users::getActiveAdminUser();
+	}
+}
